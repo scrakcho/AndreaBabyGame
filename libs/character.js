3c7ca4358510429d8aa839c339365f89f7c6dbcf
nacxo <nacxo@hotmail.com>
2016-11-17 21:12:50 +0100

    Cabecita redonda y ojitos

--- libs/character.js
+++ libs/character.js
@@ -5,62 +5,96 @@ var Character = function(args){
 	function create(args){
         'use strict';
         // Set the different geometries composing the humanoid
-        var head = new THREE.SphereGeometry(8, 8, 8, 0, Math.PI * 2, 0, Math.PI * 1),
-			body = new THREE.CubeGeometry(16, 8, 16),
-            hand = new THREE.SphereGeometry(2, 1, 1),
-            foot = new THREE.SphereGeometry(4, 1, 1, 0, Math.PI * 2, 0, Math.PI / 2),
-            nose = new THREE.SphereGeometry(1, 1, 1),
+        var head = new THREE.SphereGeometry(16, 16, 16, 0, Math.PI * 2, 0, Math.PI * 1),
+			body = new THREE.CubeGeometry(32, 16, 32),
+            hand = new THREE.SphereGeometry(4, 2, 2),
+            foot = new THREE.SphereGeometry(8, 2, 2, 0, Math.PI * 2, 0, Math.PI / 2),
+            nose = new THREE.SphereGeometry(2, 2, 2),
+			eye  = new THREE.CylinderGeometry(4, 2, 2, 40, 2 ),
+			iris = new THREE.CylinderGeometry(1, 2, 1, 20, 2 ),
             // Set the material, the "skin"
-            material = new THREE.MeshLambertMaterial(args),
-			materialPañal = new THREE.MeshPhongMaterial({color: 0xFFFFFF,emissive: 0x524e4e,specular: 0x000000,shininess: 100,shading: 'THREE.SmoothShading'});
+            materialCarne = new THREE.MeshPhongMaterial(args),
+			materialNegro = new THREE.MeshPhongMaterial({color: 0x000000}),
+			materialBlanco = new THREE.MeshPhongMaterial({color: 0xFFFFFF,emissive: 0x524e4e,specular: 0x000000,shininess: 100,shading: 'THREE.SmoothShading'});
         // Set the character modelisation object
 		character = new THREE.Object3D();
-        character.position.y = 10;
+        character.position.y = 20;
         
-		// Set and add its head		
-        head = new THREE.Mesh(head, material);
+		// Set and add its head and body		
+        head = new THREE.Mesh(head, materialCarne);
         head.position.y = 0;
-		body = new THREE.Mesh(body, materialPañal);
-		body.position.y = -6;
-		
+		body = new THREE.Mesh(body, materialBlanco);
+		body.position.y = -12;		
 		var headBSP = new ThreeBSP(head);
 		var bodyBSP = new ThreeBSP(body);
 		var head = headBSP.subtract(bodyBSP);		
-		var head = head.toMesh(material);
+		var head = head.toMesh(materialCarne);
+		head.geometry.computeVertexNormals();//Soluciona vertices no redondeados
 		var body = headBSP.intersect(bodyBSP);
-		var body = body.toMesh(materialPañal); 
+		var body = body.toMesh(materialBlanco); 
 		character.add(body);
-		
-		//character.add(body);
         character.add(head);
+		
+		// Set and add its eyes	
+		var eyes = {
+            left: new THREE.Mesh(eye, materialBlanco),
+            right: new THREE.Mesh(eye, materialBlanco)
+        };					
+		eyes.left.rotation.x = Math.PI / 2;		
+		eyes.left.position.y = 4;
+		eyes.left.position.x = -5;
+		eyes.left.position.z = 14;
+		eyes.left.rotation.z = Math.PI / 14;
+		eyes.right.rotation.x = Math.PI / 2;		
+		eyes.right.position.y = 4;
+		eyes.right.position.x = 5;
+		eyes.right.position.z = 14;
+		eyes.right.rotation.z = Math.PI / -14;
+		character.add(eyes.left);
+		character.add(eyes.right);
+		var irisLeft = new THREE.Mesh(iris, materialNegro);
+		irisLeft.rotation.x = Math.PI / 2;		
+		irisLeft.position.y = 3,5;
+		irisLeft.position.x = -4,5;
+		irisLeft.position.z = 15;
+		irisLeft.rotation.z = Math.PI / 14;
+		var irisRight = new THREE.Mesh(iris, materialNegro);
+		irisRight.rotation.x = Math.PI / 2;		
+		irisRight.position.y = 3,5;
+		irisRight.position.x = 4,5;
+		irisRight.position.z = 15;
+		irisRight.rotation.z = Math.PI / -14;
+		character.add(irisLeft);
+		character.add(irisRight);
+		
         // Set and add its hands
         hands = {
-            left: new THREE.Mesh(hand, material),
-            right: new THREE.Mesh(hand, material)
+            left: new THREE.Mesh(hand, materialCarne),
+            right: new THREE.Mesh(hand, materialCarne)
         };
-        hands.left.position.x = -10;
-        hands.left.position.y = -2;
-        hands.right.position.x = 10;
-        hands.right.position.y = -2;
+        hands.left.position.x = -20;
+        hands.left.position.y = -4;
+        hands.right.position.x = 20;
+        hands.right.position.y = -4;
         character.add(hands.left);
         character.add(hands.right);
         // Set and add its feet
         feet = {
-            left: new THREE.Mesh(foot, material),
-            right: new THREE.Mesh(foot, material)
+            left: new THREE.Mesh(foot, materialCarne),
+            right: new THREE.Mesh(foot, materialCarne)
         };
-        feet.left.position.x = -5;
-        feet.left.position.y = -12;
+        feet.left.position.x = -10;
+        feet.left.position.y = -24;
         feet.left.rotation.y = Math.PI / 4;
-        feet.right.position.x = 5;
-        feet.right.position.y = -12;
+        feet.right.position.x = 10;
+        feet.right.position.y = -24;
         feet.right.rotation.y = Math.PI / 4;
         character.add(feet.left);
         character.add(feet.right);
         // Set and add its nose
-        nose = new THREE.Mesh(nose, material);
+        nose = new THREE.Mesh(nose, materialCarne);
         nose.position.y = 0;
-        nose.position.z = 8;
+        nose.position.z = 16;
         character.add(nose);
         // Set the vector of the current motion
         var pos = new THREE.Vector3();
@@ -133,10 +167,10 @@ var CharacterMove = function(character,camara){
 	// Now let's use Sine and Cosine curves, using our "step" property ...
 	step += 1 / 2;
 	// ... to slightly move our feet and hands
-	feet.left.position.setZ(Math.sin(step) * 4);
-	feet.right.position.setZ(Math.cos(step + (Math.PI / 2)) * 4);
-	hands.left.position.setZ(Math.cos(step + (Math.PI / 2)) * 2);
-	hands.right.position.setZ(Math.sin(step) * 2);
+	feet.left.position.setZ(Math.sin(step) * 8);
+	feet.right.position.setZ(Math.cos(step + (Math.PI / 2)) * 8);
+	hands.left.position.setZ(Math.cos(step + (Math.PI / 2)) * 4);
+	hands.right.position.setZ(Math.sin(step) * 4);
 	
 } 
 var CharacterCollide = function(character){
